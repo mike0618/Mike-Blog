c71ab27dec2c19c1174ebcaf30d42e2b874c3126
Mike Zubko <mishau7@gmail.com>
2021-04-25 20:19:41 +0300

    small code optimization

--- day_14_Higher-Lower/main.py
+++ day_14_Higher-Lower/main.py
@@ -3,27 +3,27 @@ from game_data import data
 from random import choice
 # from replit import clear  # works on replit
 
+def format_data(instance):
+    name = instance['name']
+    descr = instance['description']
+    country = instance['country']
+    return f"{name}, a {descr}, from {country}"
+
 print(logo)
 score = 0
 first = choice(data)
 
 while True:
-    name_a = first['name']
-    desc_a = first['description']
-    country_a = first['country']
     count_a = first['follower_count']
 
     second = choice(data)
     while second == first:
         second = choice(data)
-    name_b = second['name']
-    desc_b = second['description']
-    country_b = second['country']
     count_b = second['follower_count']
 
-    print(f"Compare A: {name_a}, a {desc_a}, from {country_a}, {count_a} followers")
+    print("Compare A: " + format_data(first) + f", {count_a}M followers")
     print(vs)
-    print(f"Against B: {name_b}, a {desc_b}, from {country_b}")
+    print(f"Against B: " + format_data(second))
 
     answer = input("Who has more followers? Type 'A' or 'B': ").upper()
 
