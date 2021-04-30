26c09c4afe973f71d657833a85dad47e13b7e882
Mike Zubko <mishau7@gmail.com>
2021-04-30 05:08:06 +0300

    Create
    Dependencies: tkinter, turtle

---
+++ day_22_PyPong_game/main.py
@@ -0,0 +1,72 @@
+from turtle import Screen
+from paddles import Paddle, Ball, Wall, X, Y
+from board import Score, Velocity
+from tkinter import messagebox
+from random import randint
+
+
+def pong():
+    screen = Screen()
+    screen.title('PyPong Game')
+    screen.setup(X, Y)
+    screen.bgcolor('black')
+    screen.tracer(0)
+
+    r_paddle = Paddle((X // 2 - 50, 0))
+    l_paddle = Paddle((-(X // 2 - 50), 0))
+    ball = Ball()
+    scores = Score()
+    vel = Velocity()
+    upwall = Wall((0, Y // 2))
+    btmwall = Wall((0, -Y // 2))
+
+    screen.listen()
+    screen.onkey(r_paddle.up, 'Up')
+    screen.onkey(r_paddle.down, 'Down')
+    screen.onkey(l_paddle.up, 'w')
+    screen.onkey(l_paddle.down, 's')
+    screen.onkey(ball.velinc, 'Right')
+    screen.onkey(ball.veldec, 'Left')
+    screen.onkey(screen.bye, 'Escape')
+    screen.onkeypress(upwall.psyon, 'space')
+    screen.onkeyrelease(upwall.psyoff, 'space')
+
+    messagebox.showinfo('Welcome to PyPong!',
+                        'UP/DOWN: move right paddle\n'
+                        'W/S    : move left paddle\n'
+                        'RIGHT/LEFT : velocity\n'
+                        '\nSPACE : !warning! psychedelic mode\n'
+                        'can cause an epileptic seizure\n'
+                        '\nESC    : exit')
+
+    while True:
+        screen.update()
+        if upwall.psy:
+            screen.bgcolor('#' + str("%06x" % randint(0, 0xFFFFFF)))
+        ball.move()
+        bx = ball.xcor()
+        by = ball.ycor()
+        ry = r_paddle.ycor()
+        ly = l_paddle.ycor()
+        if abs(bx) == X // 2 - 70:
+            if (abs(ry - by) <= 50 and bx > 0) or (abs(ly - by) <= 50 and bx < 0):
+                ball.velinc()
+                ball.bounce_x()
+                ball.randcolor()
+                if bx > 0:
+                    r_paddle.randcolor()
+                else:
+                    l_paddle.randcolor()
+        elif abs(ball.xcor()) > X // 2:
+            ball.veldec()
+            if ball.xcor() > 0:
+                scores.lpoint()
+            else:
+                scores.rpoint()
+            ball.home()
+            ball.bounce_x()
+            scores.update()
+        vel.vupdate(ball.vel)
+
+
+pong()
