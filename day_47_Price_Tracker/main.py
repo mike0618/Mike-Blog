5b3803cf425057514550f8ad97b0a1666bf08b33
Mike Zubko <mishau7@gmail.com>
2021-07-17 09:23:47 +0300

    update

--- day_47_Price_Tracker/main.py
+++ day_47_Price_Tracker/main.py
@@ -3,6 +3,7 @@ from my_conf import BROWSER_HEADERS, EMAIL, EMAIL_TO, PWD
 from bs4 import BeautifulSoup
 import smtplib
 # import lxml
+
 url = 'https://www.amazon.com/ASUS-i7-10750H-ScreenPad-Celestial-UX581LV-XS74T/dp/B08D941WH6/' \
       'ref=sr_1_1?dchild=1&keywords=ASUS%2BZenBook%2BPro%2BDuo%2BUX581&qid=1626358964&sr=8-1&th=1'
 amzn_page = requests.get(url, headers=BROWSER_HEADERS).text
@@ -15,8 +16,8 @@ title = soup.find('span', id='productTitle').getText().strip()
 if price < 2620:
     msg = f'Subject:Amazon price alert!\n\n{title}\nnow ${price}\n{url}'
     with smtplib.SMTP('smtp.mail.yahoo.com') as conn:
-                conn.starttls()
-                conn.login(user=EMAIL, password=PWD)
-                conn.sendmail(from_addr=EMAIL,
-                              to_addrs=EMAIL_TO,
-                              msg=msg.encode('utf-8'),)
+        conn.starttls()
+        conn.login(user=EMAIL, password=PWD)
+        conn.sendmail(from_addr=EMAIL,
+                      to_addrs=EMAIL_TO,
+                      msg=msg.encode('utf-8'),)
