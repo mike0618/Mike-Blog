d218e1533034757c8f6fce24b7d1ac6a5988ba90
Mike Zubko <mishau7@gmail.com>
2021-10-25 13:45:06 +0300

    Initial commit

--- bigO.py
+++ bigO.py
@@ -5,18 +5,25 @@ from scipy.special import gamma
 plt.style.use('dark_background')
 
 # set up runtime comparisons
-n = np.linspace(1, 10)
-labels = ['Constant', 'Logarithmic', 'Linear', 'Log Linear', 'Quadratic', 'Cubic', 'Exponencial', 'Factorial']
-big_o = [np.ones(n.shape), np.log(n), n, n * np.log(n), n ** 2, n ** 3, 2 ** n, gamma(n + 1)]
+n = np.linspace(1, 100)
+big_o = {'Constant': np.ones(n.shape),
+         'Logarithmic': np.log(n),
+         'Linear': n,
+         'Log Linear': n * np.log(n),
+         'Quadratic': n ** 2,
+         'Cubic': n ** 3,
+         'Exponential': 2 ** n,
+         'Factorial': gamma(n + 1)}
 
 # plot setup
-plt.figure(figsize=(12, 10))
+plt.figure(figsize=(10, 6))
 plt.ylim(0, 1000)
 
-for i in range(len(big_o)):
-    plt.plot(n, big_o[i], label=labels[i])
+for k, v in big_o.items():
+    plt.plot(n, v, label=k)
 
 plt.legend(loc=0)
 plt.ylabel('Relative Runtime')
 plt.xlabel('n')
+plt.tight_layout()
 plt.show()
