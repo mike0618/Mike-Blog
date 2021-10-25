bed2f3f4d3e0c5aa0618fb17ea6de4c968bc916b
Mike Zubko <mishau7@gmail.com>
2021-10-25 20:51:43 +0300

    Initial commit

--- bigO.py
+++ bigO.py
@@ -5,7 +5,7 @@ from scipy.special import gamma
 plt.style.use('dark_background')
 
 # set up runtime comparisons
-n = np.linspace(1, 100)
+n = np.linspace(1, 50)
 big_o = {'Constant': np.ones(n.shape),
          'Logarithmic': np.log(n),
          'Linear': n,
@@ -16,8 +16,9 @@ big_o = {'Constant': np.ones(n.shape),
          'Factorial': gamma(n + 1)}
 
 # plot setup
-plt.figure(figsize=(10, 6))
+fig = plt.figure(figsize=(20, 11))
 plt.ylim(0, 1000)
+plt.xlim(0, 50)
 
 for k, v in big_o.items():
     plt.plot(n, v, label=k)
@@ -26,4 +27,14 @@ plt.legend(loc=0)
 plt.ylabel('Relative Runtime')
 plt.xlabel('n')
 plt.tight_layout()
+plt.get_current_fig_manager().set_window_title('Big O Notation')
+plt.margins(x=0)
+fig.subplots_adjust(
+    top=0.99,
+    bottom=0.05,
+    left=0.04,
+    right=0.99,
+    hspace=0.2,
+    wspace=0.2
+)
 plt.show()
