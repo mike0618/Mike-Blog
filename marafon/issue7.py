1f10d9f6749644dff88c9e04b38a782f1349f6a1
Mike Zubko <mishau7@gmail.com>
2021-11-01 11:57:13 +0300

    Small fix No recursion version added

--- marafon/issue7.py
+++ marafon/issue7.py
@@ -1,3 +1,6 @@
+from itertools import product
+
+
 def count_animals(txt: str) -> int:
     animals = ["dog", "cat", "bat", "cock", "cow", "pig", "fox", "ant", "bird", "lion", "wolf", "deer", "bear", "frog",
                "hen", "mole", "duck", "goat"]
@@ -31,11 +34,62 @@ def count_animals(txt: str) -> int:
         if len(m_anm) > len(max_animals):
             max_animals.clear()
             max_animals.extend(m_anm)
-        m_anm.clear()
+        if m_anm:
+            m_anm.pop()
 
     counter(animals, letters)
     print(max_animals)
     return len(max_animals)
+
+
+def count_animals2(txt):
+    animals = ["dog", "cat", "bat", "cock", "cow", "pig", "fox", "ant", "bird", "lion", "wolf", "deer", "bear", "frog",
+               "hen", "mole", "duck", "goat"]
+
+    def counter(anml):
+        exist = True
+        ltrs = list(txt)
+        count = 0
+        while exist:
+            for ltr in anml:
+                if ltr not in ltrs:
+                    exist = False
+                    break
+                ltrs.remove(ltr)
+            else:
+                count += 1
+        return count
+
+    def check(d: dict):
+        strn = ''
+        tmp = list(txt)
+        for k, v in d.items():
+            strn += k * v
+        if len(strn) <= len(txt):
+            for ltr in strn:
+                if ltr not in tmp:
+                    return False
+                tmp.remove(ltr)
+            return True
+        return False
+
+    counted = {}
+    for a in animals:
+        num = counter(a)
+        if num:
+            counted[a] = num
+
+    for n in range(sum(counted.values())):
+        for p in product(range(max(counted.values()) + 1), repeat=len(counted)):
+            if sum(p) == n:
+                dict_counted = counted.copy()
+                for i, a in enumerate(dict_counted):
+                    if p[i] <= dict_counted[a] and p[i]:
+                        dict_counted[a] -= p[i]
+                if check(dict_counted):
+                    result = {k: v for k, v in dict_counted.items() if v}
+                    print(result)
+                    return sum(result.values())
 
 
 if __name__ == '__main__':
@@ -43,3 +97,10 @@ if __name__ == '__main__':
     print(count_animals("cockdogwdufrbir"))
     print(count_animals("dogdogdogdogdog"))
     print(count_animals('acatdgcatogocatdoggoatdct'))
+    print(count_animals('cockdogwdufrbiraier' * 3))
+
+    print(count_animals2('goatcode'))
+    print(count_animals2("cockdogwdufrbir"))
+    print(count_animals2("dogdogdogdogdog"))
+    print(count_animals2('acatdgcatogocatdoggoatdct'))
+    print(count_animals2('cockdogwdufrbiraier' * 3))
